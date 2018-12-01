1420500ce5e456820ae774b7b019011d440dd96f
Teerapat Tee <teerapat12345678@gmail.com>
2018-12-01 12:01:37 +0700

    use varialbles

--- tensorflow/variables.py
+++ tensorflow/variables.py
@@ -1,1 +1,15 @@
-    
+import tensorflow as tf
+
+x = tf.Variable(10, name='x')
+y = tf.Variable(20, name='y')
+
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('./graphs', session.graph)
+    session.run(tf.global_variables_initializer())
+
+    for _ in range(10):
+        session.run(tf.add(x, y))
+
+    print(tf.get_default_graph().as_graph_def())
+
+writer.close()
