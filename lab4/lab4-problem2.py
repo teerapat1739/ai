c81270f1c22da540c71b700d73755b6bd13dafbc
Teerapat Tee <teerapat12345678@gmail.com>
2019-01-12 16:21:33 +0700

    solve lab2

--- lab4/lab4-problem2.py
+++ lab4/lab4-problem2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import normalize
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 '''
 Step 1: Read the dataset using pandas.
@@ -32,7 +34,7 @@ dataframe = pokemon_dataset[pokemon_dataset['type2'].isnull()].loc[
 
 # Grap only 'sp_attack', ..., 'hp' as an input X
 # To index by position in pandas, see https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.iloc.html
-X = # Put your code here to construct feature matrix X
+X = dataframe.iloc[:, :-1].values
 
 # Normalizing is not necessary for the classification; but, it will make visualizing task (easier for our eyes).
 # So, let's do this since we will also visualize it at the end of this exercise !
@@ -40,25 +42,53 @@ X = # Put your code here to construct feature matrix X
 X_normalized = normalize(X)
 
 # Grap the last column as a target y
-y = # Put your code here to construct target vector y
+y = dataframe.iloc[:, -1].values
 
 '''
 Step 3: Fit linear discriminant analysis model according to the given training data. 
 See https://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html
 '''
 
-linearDiscriminantAnalysis = LinearDiscriminantAnalysis()
-# Try to read the document given above by yourself to train the mode 
+linearDiscriminantAnalysis = LinearDiscriminantAnalysis(n_components=3)
+linearDiscriminantAnalysis.fit(X_normalized, y) # Now, we have trained with LDA
 
 '''
 Step 4: Show the predicted type for each pokemon and measure the accuracy. 
 To predict class labels for samples in X, use method 'predict'
 '''
+predicted_type = linearDiscriminantAnalysis.predict(X_normalized)
+print("Predicted type of each pokemon is:")
+print(predicted_type,'\n')
 
-# Try to read the document given in step 3 to make prediction
+print("Actual type of each pokemon is:")
+print(y, '\n')
 
-# After that, write codes to: 
-# 1) show the predicted type of each pokemon
-# 2) show the actual pokemon of each pokeon
-# 3) show numerical value representing its accuracy
-# Noted that there may be more than one line of code for this step 
+accuracy = sum(predicted_type == y) / len(y)
+print("Accuracy is {0:f}".format(accuracy))
+
+'''
+Step 5: Now, we want to understand why we have reached such degree of accuracy. 
+Since we can plot only 2D or 3D, we need to reduce the dimensions. We will come back to 
+the problem of dimensionality reduction later in this course !
+'''
+
+X_projected = linearDiscriminantAnalysis.fit_transform(X, y)
+
+colors = mpl.cm.get_cmap(name='tab20').colors
+categories = pd.Categorical(pd.Series(y)).categories
+ret = pd.DataFrame(
+    {'D1': X_projected[:, 0], 'D2': X_projected[:, 1], 'Type': pd.Categorical(pd.Series(y))}
+)
+
+fig, ax = plt.subplots(1, figsize=(12, 6))
+
+for col, cat in zip(colors, categories):
+    (ret
+         .query('Type == @cat')
+         .plot.scatter(x='D1', y='D2', color=col, label=cat, ax=ax,
+                       s=100, edgecolor='black', linewidth=1,
+                       title='Two-Component LDA Decomposition')
+         .legend()
+    )
+
+# Answer whether the classifying pokemon using stats alone is linearly separable ?
